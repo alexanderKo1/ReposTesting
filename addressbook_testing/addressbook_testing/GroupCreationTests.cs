32e0ed10b4dc4e508ce5f5384e5e78600aee06aa
alexanderKo1 <koalexander988@gmail.com>
2021-10-16 12:13:11 +0300

    12

--- addressbook_testing/addressbook_testing/GroupCreationTests.cs
+++ addressbook_testing/addressbook_testing/GroupCreationTests.cs
@@ -10,7 +10,7 @@ using OpenQA.Selenium.Support.UI;
 namespace addressbook_testing
 {
     [TestFixture]
-    public class UntitledTestCase
+    public class GroupCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
@@ -40,7 +40,7 @@ namespace addressbook_testing
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void GroupCreationTest()
         {
             driver.Navigate().GoToUrl(baseURL);
             driver.FindElement(By.Name("user")).Click();
