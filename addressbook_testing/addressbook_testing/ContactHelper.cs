ea52cb63d80d245509167a20f9524f17b4832e9e
alexanderKo1 <koalexander988@gmail.com>
2021-10-23 22:19:25 +0300

    3

--- addressbook_testing/addressbook_testing/ContactHelper.cs
+++ addressbook_testing/addressbook_testing/ContactHelper.cs
@@ -13,7 +13,7 @@ namespace addressbook_testing
     public class ContactHelper : HelperBase
     {
         public ContactHelper(IWebDriver driver) : base(driver) { }
-        //EntryCreationTest 1
+        //EntryCreationTest 13
         public void SubmitNewEntry()
         {
             driver.FindElement(By.Name("submit")).Click();
