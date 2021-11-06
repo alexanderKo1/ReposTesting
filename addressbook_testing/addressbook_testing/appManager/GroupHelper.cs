2725016face26b688482afb925cc719918ecdd94
alexanderKo1 <koalexander988@gmail.com>
2021-11-06 09:27:12 +0300

    реализован 1й способ ускорения в группах

--- addressbook_testing/addressbook_testing/appManager/GroupHelper.cs
+++ addressbook_testing/addressbook_testing/appManager/GroupHelper.cs
@@ -24,18 +24,21 @@ namespace addressbook_testing
             ReturnToGroupsPage();
             return this;
         }
-
+        private List<GroupData> groupCache = null;
         public List<GroupData> GetGroupList()
         {
-            List<GroupData> groups = new List<GroupData>();
-            manager.Navigator.GoToGroupsPage();
-            ICollection<IWebElement> elements = driver.FindElements(By.CssSelector("span.group"));
-            foreach (IWebElement element in elements)
+            if (groupCache == null)
             {
-                groups.Add(new GroupData(element.Text));
+                groupCache = new List<GroupData>();
+                List<GroupData> groups = new List<GroupData>();
+                manager.Navigator.GoToGroupsPage();
+                ICollection<IWebElement> elements = driver.FindElements(By.CssSelector("span.group"));
+                foreach (IWebElement element in elements)
+                {
+                    groupCache.Add(new GroupData(element.Text));
+                }
             }
-
-            return groups;
+            return new List <GroupData>(groupCache);
         }
 
         public GroupHelper Modify(int v, GroupData newData) //Метод модификации группы
@@ -78,6 +81,7 @@ namespace addressbook_testing
         public GroupHelper RemoveGroup()
         {
             driver.FindElement(By.Name("delete")).Click();
+            groupCache = null;
             return this;
         }
 
@@ -103,11 +107,13 @@ namespace addressbook_testing
         public GroupHelper SubmitGroupCreation()
         {
             driver.FindElement(By.Name("submit")).Click();
+            groupCache = null;
             return this;
         }
         public GroupHelper SubmitGroupModification()
         {
             driver.FindElement(By.Name("update")).Click();
+            groupCache = null;
             return this;
         }
         public GroupHelper InitGroupModification()
