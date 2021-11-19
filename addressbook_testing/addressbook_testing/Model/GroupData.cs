c4ae6c3312dd0f13b4ef1929274bbcc95e6fed98
alexanderKo1 <koalexander988@gmail.com>
2021-11-19 21:29:52 +0300

    Запись в xml, json

--- addressbook_testing/addressbook_testing/Model/GroupData.cs
+++ addressbook_testing/addressbook_testing/Model/GroupData.cs
@@ -8,6 +8,9 @@ namespace addressbook_testing
 {
     public class GroupData : IEquatable<GroupData>, IComparable<GroupData>
     {
+        public GroupData()
+        {
+        }
         public GroupData(string name)
         {
             Name = name;
