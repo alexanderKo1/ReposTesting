e2e18826f5fa39701b953477e91a81e34c6854b0
alexanderKo1 <koalexander988@gmail.com>
2021-12-04 14:16:12 +0300

    HW 18 Completed

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public static string DELETEGROUPWINTITLE = "Delete group";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+
+            OpenGroupsDialog();
+
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51"
+                , "GetItemCount", "#0", "");
+
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51"
+                , "Gettext", "#0|#" + i, "");
+
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            }
+
+            CloseGroupsDialog();
+
+            return list;
+        }
+
+        internal void GroupRemoving(int indexer)
+        {
+            OpenGroupsDialog();
+
+            //1. выбор элемента
+            aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.2c908d51"
+                , "Select", "#0|#" + indexer, "");
+
+            //2. Нажатие на кнопку 'Delete'
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");
+
+            // Ожидание окна
+            aux.WinWaitActive(GROUPWINTITLE, "", 5);
+
+            //3. Выбор второго чек-бокса
+            aux.ControlClick(DELETEGROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d51");
+
+            //4. Нажатие кнопки ОК
+            aux.ControlClick(DELETEGROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53");
+
+            // Ожидание окна
+            aux.WinWaitActive(DELETEGROUPWINTITLE, "", 5);
+
+            CloseGroupsDialog();
+        }
+
+        internal void GroupRemovingCondition()
+        {
+            if (GetGroupList().Count == 1)
+            {
+                GroupData newGroup = new GroupData() { Name = "GroupForRemoving" };
+
+                OpenGroupsDialog();
+                aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53"); //-11.38
+                aux.Send(newGroup.Name);
+                aux.Send("{ENTER}");
+                CloseGroupsDialog();
+            }
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            OpenGroupsDialog();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d53"); //-11.38
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            CloseGroupsDialog();
+        }
+
+        private void CloseGroupsDialog()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d54");
+        }
+
+        private void OpenGroupsDialog()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.2c908d512");
+            aux.WinWait(GROUPWINTITLE);
+        }
+        public void GroupMonitor(List<GroupData> groups)
+        {
+            int nn = 0;
+            foreach (GroupData element in groups)
+            {
+                System.Console.Out.Write((++nn) + " | " + element.Name);
+            }
+            System.Console.Out.Write("FINISHED" + "\n");
+        }
+    }
+}
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoving() // ДЗ 18
+        {
+            System.Console.Out.Write(app.Groups.GetGroupList().Count);
+
+            app.Groups.GroupRemovingCondition();
+
+            int indexer = 0;
+
+            List<GroupData> checkGroups = app.Groups.GetGroupList();
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+            app.Groups.GroupRemoving(indexer);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+
+            oldGroups.RemoveAt(indexer);
+
+            oldGroups.Sort();
+            newGroups.Sort();
+
+            System.Console.WriteLine((checkGroups.Count - 1) + " and " + app.Groups.GetGroupList().Count);
+            Assert.AreEqual(checkGroups.Count - 1, app.Groups.GetGroupList().Count);
+
+            Assert.AreEqual(oldGroups, newGroups);
+            app.Groups.GroupMonitor(oldGroups);
+            app.Groups.GroupMonitor(newGroups);
+        }
+    }
+}
