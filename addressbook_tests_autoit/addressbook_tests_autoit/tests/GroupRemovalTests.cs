4ad312ec8b833c73e7eb2218b3c1166d8ea72874
alexanderKo1 <koalexander988@gmail.com>
2021-12-04 11:28:38 +0300

    Автоматизация тестирования десктопного приложения. Добавление группы

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+/*
+namespace addressbook_tests_autoit
+{
+[TestFixture]
+public class GroupRemovalTests : TestBase
+{
+
+   [Test]
+   public void TestGroupRemoving()
+   {
+       List<GroupData> oldGroups = app.Groups.GetGroupList();
+
+
+       GroupData newGroup = new GroupData()
+       {
+           Name = "test"
+       };
+
+       app.Groups.Add(newGroup);
+
+       List<GroupData> newGroups = app.Groups.GetGroupList();
+       oldGroups.Add(newGroup);
+       oldGroups.Sort();
+       newGroups.Sort();
+
+       Assert.AreEqual(oldGroups, newGroups);
+
+   }
+}
+}
+   */
